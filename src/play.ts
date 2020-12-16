0dbf3e1942f5259bfd1cf25169dd5c62517034f5
Pho Nguyen <pho.nguyen@vn.woowahan.com>
2020-12-17 01:13:40 +0700

    Using mongoose

--- src/play.ts
+++ src/play.ts
@@ -1,21 +1,35 @@
+import * as _ from 'lodash'
 import * as graphql from 'graphql'
 import { ApolloServer } from 'apollo-server'
+import { Document } from 'mongoose'
+import { mongoose } from './mongoose'
 
-const options = [
-  { id: 'option_1', name: 'Nuoc mam' },
-  { id: 'option_2', name: 'Op la' },
-  { id: 'option_3', name: 'Hop giay' },
-]
+interface IDish {
+  _id: string
+  name: string
+  optionIds: string[]
+}
 
-const dishes = [
-  { id: 'dish_1', name: 'Com tam', optionIds: ['option_1'] },
-  { id: 'dish_2', name: 'Pho', optionIds: ['option_2', 'option_3'] },
-]
+interface IOption {
+  _id: string
+  name: string
+}
+
+export const Dish = mongoose.model<IDish & Document>('Dish', new mongoose.Schema({
+  _id: String,
+  name: String,
+  optionIds: [String],
+}))
+
+export const Option = mongoose.model<IOption & Document>('Option', new mongoose.Schema({
+  _id: String,
+  name: String,
+}))
 
 const option = new graphql.GraphQLObjectType({
   name: 'Option',
   fields: {
-    id: { type: graphql.GraphQLString },
+    _id: { type: graphql.GraphQLString },
     name: { type: graphql.GraphQLString },
   }
 })
@@ -23,12 +37,12 @@ const option = new graphql.GraphQLObjectType({
 const dish = new graphql.GraphQLObjectType({
   name: 'Dish',
   fields: {
-    id: { type: graphql.GraphQLString },
+    _id: { type: graphql.GraphQLString },
     name: { type: graphql.GraphQLString },
     optionIds: { type: graphql.GraphQLList(graphql.GraphQLString) },
     options: {
       type: graphql.GraphQLList(option),
-      resolve: (parent) => options.filter(option => parent.optionIds.includes(option.id)),
+      resolve: (parent) => Option.find({ _id: { $in: parent.optionIds } }),
     },
   }
 })
@@ -38,17 +52,31 @@ const query = new graphql.GraphQLObjectType({
   fields: {
     dishes: {
       type: graphql.GraphQLList(dish),
-      resolve: () => dishes,
+      resolve: () => Dish.find({}),
     },
     dish: {
       type: dish,
       args: {
-        id: { type: graphql.GraphQLString }
+        _id: { type: graphql.GraphQLString }
       },
-      resolve: (_, { id }) => dishes.find(dish => dish.id === id),
+      resolve: (_, { _id }) => Dish.findById(_id),
     }
   }
 })
+
+async function init() {
+  const exists = !_.isNil(await Dish.findOne({}))
+  if (exists) return
+  await Option.insertMany([
+    { _id: 'option_1', name: 'Nuoc mam' },
+    { _id: 'option_2', name: 'Op la' },
+    { _id: 'option_3', name: 'Hop giay' },
+  ])
+  await Dish.insertMany([
+    { _id: 'dish_1', name: 'Com tam', optionIds: ['option_1'] },
+    { _id: 'dish_2', name: 'Pho', optionIds: ['option_2', 'option_3'] },
+  ])
+}
 
 const schema = new graphql.GraphQLSchema({ query })
 
@@ -56,4 +84,5 @@ const server = new ApolloServer({ schema })
 
 server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
+  return init()
 })
