b50e5286e817c21a9183d75c3f88212f22f10abb
Pho Nguyen <pho.nguyen@vn.woowahan.com>
2021-02-10 20:17:06 +0700

    Test getGraphqlApis

--- src/core/model-builder.test.ts
+++ src/core/model-builder.test.ts
@@ -3,6 +3,7 @@ import * as graphql from 'graphql'
 import { expect } from 'chai'
 import { TestUtils } from '../helpers'
 import { ModelBuilder } from './model-builder'
+import { ApiGenerator } from './api-generator'
 
 describe(TestUtils.getTestTitle(__filename), () => {
   it('#getDbModel', async () => {
@@ -23,5 +24,18 @@ describe(TestUtils.getTestTitle(__filename), () => {
     const { _id } = await User.create({ email: 'abcd@gmail.com' })
     expect(await User.findById(_id)).to.include({ email: 'abcd@gmail.com' })
     await User.deleteMany({})
+  })
+
+  it('#getGraphqlApis', () => {
+    const modelBuilder = new ModelBuilder(<any> 'sample_model') 
+
+    td.replace(modelBuilder, 'getDbModel')
+    td.replace(ApiGenerator, 'generate')
+    td.when(ApiGenerator.generate(
+      td.matchers.anything(),
+      td.matchers.anything()
+    )).thenReturn(<any>'_api_generators')
+
+    expect(modelBuilder.getGraphqlApis()).to.equal('_api_generators')
   })
 })
