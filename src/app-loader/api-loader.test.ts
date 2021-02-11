6c696ed644983fc611011af244a8ec72fb8b97a7
Pho Nguyen <pho.nguyen@vn.woowahan.com>
2021-02-11 08:42:40 +0700

    Test #getPath

--- src/app-loader/api-loader.test.ts
+++ src/app-loader/api-loader.test.ts
@@ -1,17 +1,27 @@
+import * as td from 'testdouble'
 import * as path from 'path'
 import { expect } from 'chai'
 import { TestUtils } from '../helpers'
 import { ApiLoader } from './api-loader'
 
 describe(TestUtils.getTestTitle(__filename), () => {
-  describe('#getApis', () => {
-    it('Can get apis', async () => {
-      const folder = path.join(__dirname, 'just-for-test-only')
-      const fileA = path.join(folder, 'a.ts')
-      const fileB = path.join(folder, 'b.ts')
-      const fileC = path.join(folder, 'c.ts')
-      const apis = await ApiLoader.getApis([fileA, fileB, fileC])
-      expect(apis).to.deep.equal([1, 2, 3, 4])
-    })
+  it('#getApis', async () => {
+    const folder = path.join(__dirname, 'just-for-test-only')
+    const fileA = path.join(folder, 'a.ts')
+    const fileB = path.join(folder, 'b.ts')
+    const fileC = path.join(folder, 'c.ts')
+    const apis = await ApiLoader['getApis']([fileA, fileB, fileC])
+    expect(apis).to.deep.equal([1, 2, 3, 4])
+  })
+
+  it('#getPaths', () => {
+    const testPath = path.join(__dirname, 'just-for-test-only')
+    td.replace(ApiLoader, 'MODEL_FOLDER', testPath)
+    expect(ApiLoader['getPaths']())
+      .to.deep.equal([
+        path.join(testPath, 'a.ts'),
+        path.join(testPath, 'b.ts'),
+        path.join(testPath, 'c.ts'),
+      ])
   })
 })
--- src/app-loader/api-loader.ts
+++ src/app-loader/api-loader.ts
@@ -1,12 +1,21 @@
 import * as _ from 'lodash'
+import * as path from 'path'
+import * as fs from 'fs'
 
 export class ApiLoader {
-  static async getApis(paths: string[]) {
+  private static async getApis(paths: string[]) {
     const importedObjects = await Promise.all(_.map(paths, path => import(path)))
     return _.chain(importedObjects)
       .map('apis')
       .compact()
       .reduce((a, b) => _.concat(a, b))
       .value()
+  }
+
+  private static MODEL_FOLDER = path.join('..', 'models')
+
+  private static getPaths() {
+    return fs.readdirSync(this.MODEL_FOLDER)
+      .map(file => path.join(this.MODEL_FOLDER, file))
   }
 }
