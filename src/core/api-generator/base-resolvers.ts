d707795fefa987a72b0a2e1a808313fa9bf24154
Pho Nguyen <pho.nguyen@vn.woowahan.com>
2021-03-04 14:47:33 +0700

    Connect to mongodb server in test

--- src/core/api-generator/base-resolvers.ts
+++
@@ -1,7 +0,0 @@
-import { ModelOf } from '../metadata'
-
-export const enrichModel = <T, Context>(Model: ModelOf<T, Context>) => {
-  return class extends (Model as any) {
-    create
-  }
-}
--- src/init.test.ts
+++ src/init.test.ts
@@ -2,6 +2,11 @@ import * as td from 'testdouble'
 import * as mongoose from 'mongoose'
 import { TypeGenerator } from './core'
 
+before(() => mongoose.connect(
+  process.env.DATABASE_URL as string,
+  { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
+))
+
 beforeEach(() => {
   td.reset()
   TypeGenerator['_outputTypes'] = {}
