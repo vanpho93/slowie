0658defaf3385171e0fb2092e42845027a342015
Pho Nguyen <pho.nguyen@vn.woowahan.com>
2021-03-10 17:34:40 +0700

    continue

--- src/core/list-api-builder.ts
+++ src/core/list-api-builder.ts
@@ -1,34 +1,34 @@
-import { IApiGenerator } from './metadata'
+// import { IApiGenerator } from './metadata'
 
-enum ESearchOperation {
-  EQUAL = '$eq',
-}
+// enum ESearchOperation {
+//   EQUAL = '$eq',
+// }
 
-interface ISearchableProperty {
-  fieldName: string
-  operator: ESearchOperation
-  required: boolean
-}
+// interface ISearchableProperty {
+//   fieldName: string
+//   operator: ESearchOperation
+//   required: boolean
+// }
 
-class ListAPIBuilder {
-  private name: string
-  searchableProps: ISearchableProperty[] = []
+// class ListAPIBuilder {
+//   private name: string
+//   searchableProps: ISearchableProperty[] = []
 
-  from() {
-    return this
-  }
+//   from() {
+//     return this
+//   }
 
-  setApiName(name: string) {
-    this.name = name
-    return this
-  }
+//   setApiName(name: string) {
+//     this.name = name
+//     return this
+//   }
 
-  add(searchableProp: ISearchableProperty) {
-    this.searchableProps.push(searchableProp)
-    return this
-  }
+//   add(searchableProp: ISearchableProperty) {
+//     this.searchableProps.push(searchableProp)
+//     return this
+//   }
 
-  build(): IApiGenerator | null {
-    return null
-  }
-}
+//   build(): IApiGenerator | null {
+//     return null
+//   }
+// }
